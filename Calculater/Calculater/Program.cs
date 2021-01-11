2ec3e4ce949470227acdfbcf6915f0868c5e4021
Karambot-Dev <73456788+KaramBotStudios-DEV1@users.noreply.github.com>
2021-01-11 10:34:10 +0700

    Update Program.cs

--- Calculater/Calculater/Program.cs
+++ Calculater/Calculater/Program.cs
@@ -1,18 +1,18 @@
-﻿using System;
+﻿using System; //check what this does from Form1.cs
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Calculater
+namespace Calculater //names the app
 {
-    static class Program
+    static class Program //program class
     {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main() //starts the form[application]
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
